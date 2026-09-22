5f2edf5bacc4a0885f889fb9b6e5e8e2b7ec5ae3
Shashwat-2121 <Shashwat.39008@stu.upes.ac.in>
2026-09-22 11:26:56 +0530

    Add basic arithmetic operations in 2.c

---
+++ 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("Sum=%d, Diff=%d, Product=%d", a + b, a - b, a * b);
+
+    if (b != 0)
+        printf(", Quotient=%d", a / b);
+    else
+        printf(", Quotient=undefined");
+
+    return 0;
+}
